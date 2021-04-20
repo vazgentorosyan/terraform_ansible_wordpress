d6694ff52b72258ebadd0e07a0066def48f1b985
Vazgen Torosyan <vazgentorosyan@gmail.com>
2021-04-20 22:53:24 +0400

    up instance and apply security group

---
+++ wordpress.tf
@@ -0,0 +1,58 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_security_group" "allow_http_and_ssh" {
+  name        = "allow_http_and_ssh"
+  description = "Allow web and ssh inbound traffic"
+  vpc_id      = "vpc-700fbd0d"
+
+  ingress {
+    description      = "http from anywere"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+  ingress {
+    description      = "https from anywere"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  ingress {
+    description      = "ssh from anywere"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
+
+resource "aws_instance" "wordpress" {
+  ami           = "ami-096fda3c22c1c990a"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.allow_http_and_ssh.id}"]
+
+}
